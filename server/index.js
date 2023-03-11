1ee774930a311f77ce24f6e932c24f654e8b6f54
Sameer-321 <snrsameer1@gmail.com>
2023-03-11 12:13:31 +0545

    mongoDB connection, env, nodemon, and so on

--- server/index.js
+++ server/index.js
@@ -1,6 +1,32 @@
-import express from "express"
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 
-app.listen(9000,()=>{
-    console.log("Connected to Back-End")
+const app = express();
+dotenv.config();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connnected to mongoDB")
+  } catch (error) {
+    throw error;
+  }
+};
+
+mongoose.connection.on("disconnected",()=>{
+    console.log("mongoDB disconnected")
 })
+mongoose.connection.on("connected",()=>{
+    console.log("mongoDB connected")
+})
+
+app.get("/",(req,res)=>{
+    res.send("hello first request")
+})
+
+app.listen(9000, () => {
+  connect();
+  console.log("Connected to Back-End");
+});
+ 
